3a00946336babe2ead2dc79a84aa106300117393
Alex Williams <alex.williams@skyscanner.net>
2020-09-05 16:22:31 +0100

    Use gorilla handlers for better logging

--- pkg/highheath/http.go
+++ pkg/highheath/http.go
@@ -21,13 +21,6 @@ func init() {
 	githubClient = GithubClient()
 }
 
-func LogRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
-}
-
 func HandleHealth(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
