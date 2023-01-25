20581cbd8171099774f7bc4b5b7f35b1e3667a25
Alex Williams <alex.williams@skyscanner.net>
2023-01-25 23:19:18 +0000

    Add graceful shutdown on interupt or SIGTERM

--- cmd/highheath/main.go
+++ cmd/highheath/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/handlers"
@@ -36,6 +39,28 @@ func main() {
 	handler.Handle("/api/contact", &highheath.Handler{AppContext: ac, H: highheath.HandleContactForm})
 	handler.Handle("/api/comment", &highheath.Handler{AppContext: ac, H: highheath.HandleCommentForm})
 	handler.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Println("Starting server on 0.0.0.0:8080")
-	server.ListenAndServe()
+
+	// Setup a context which will be notified on SIGINT to capture user interupts
+	// when going through the authorization flow
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Println("Starting server on 0.0.0.0:8080")
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	// Gracefully (15s) shutdown servers
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	log.Println("Shutdown down servers with 15s timeout")
+	err := server.Shutdown(ctx)
+	if err != nil {
+		log.Fatalf("Http Server Shutdown error: %v", err)
+	}
+	log.Println("Shutdown complete")
 }
